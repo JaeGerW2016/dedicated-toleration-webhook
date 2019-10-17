24f1bbf7b261ae6d9332fe60a7a2a2b25af29cd0
jasper <314315960@qq.com>
2019-10-17 14:04:04 +0800

    add deployment

---
+++ deployment/patch-ca-bundle.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ROOT=$(cd $(dirname $0)/../../; pwd)
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
+
+cat <<EOF  >> k8s/mutatingwebhook-ca-bundle.yaml
+apiVersion: admissionregistration.k8s.io/v1beta1
+kind: MutatingWebhookConfiguration
+metadata:
+  name: dedicated-toleration-webhook
+  labels:
+    app: dedicated-toleration-webhook
+webhooks:
+  - name: dtw.webhook.io
+    clientConfig:
+      service:
+        name: dedicated-toleration-webhook
+        namespace: default
+        path: "/apply-dtw"
+      caBundle: ${CA_BUNDLE}
+    rules:
+      - operations: [ "CREATE" ]
+        apiGroups: [""]
+        apiVersions: ["v1"]
+        resources: ["pods"]
+      - operations: [ "CREATE" ]
+        apiGroups: ["apps"]
+        apiVersions: ["v1"]
+        resources: ["deployments"]
+EOF
