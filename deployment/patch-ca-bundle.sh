d3c22321146da35793bcf465b397cf25ae45a23d
jasper <314315960@qq.com>
2019-10-17 14:39:15 +0800

    v1.0

--- deployment/patch-ca-bundle.sh
+++ deployment/patch-ca-bundle.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
 
-cat <<EOF  >> k8s/mutatingwebhook-ca-bundle.yaml
+cat <<EOF  >> ../k8s/mutatingwebhook-ca-bundle.yaml
 apiVersion: admissionregistration.k8s.io/v1beta1
 kind: MutatingWebhookConfiguration
 metadata:
